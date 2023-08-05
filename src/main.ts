bd5c5a7f85a39f7d3f02e8a53335dc3d4ba76abd
JalluMerlin <merlin.jallu@epitech.eu>
2023-08-05 18:41:13 +0200

    :(

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
     origin: ['http://localhost:8080', 'https://book-paps.netlify.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
