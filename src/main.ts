82a013f16ad2fb02fe1e4504f217779440b56651
JalluMerlin <merlin.jallu@epitech.eu>
2023-08-05 19:06:50 +0200

    *

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'https://book-paps.netlify.app',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
   await app.listen(process.env.PORT || 3000);
