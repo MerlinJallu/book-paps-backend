40ddd40d050095ab277b3b267be3da256f70c3e8
JalluMerlin <merlin.jallu@epitech.eu>
2023-08-05 18:34:05 +0200

    deploy

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://localhost:8080',
+    origin: ['http://localhost:8080', 'https://book-paps.netlify.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
-  await app.listen(port);
+  await app.listen(3000);
 }
 bootstrap();
